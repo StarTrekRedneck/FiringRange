ed47ecfd2cdcf4ddcd22c52011c4f3fa58e5490f
StarTrekRedneck <dippykdog@gmail.com>
2015-09-30 23:28:45 -0500

    first commit to feature branch

---
+++ WebApplication1/src/WebApplication1/Areas/MyOrders/Controllers/MyOrdersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace WebApplication1.Areas.MyOrders.Controllers
+{
+    [Route("api/myorders/")]
+    public class MyOrdersController : Controller
+    {
+        // GET: api/myorders
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "order1", "order2" };
+        }
+
+        // GET api/orders/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "order" + id;
+        }
+    }
+}
