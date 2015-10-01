5ee93cf49b02e7c4c956f7fbc4ed8cc3117919e9
StarTrekRedneck <dippykdog@gmail.com>
2015-09-30 23:54:15 -0500

    Converted MyOrdersController to use new Order DTO

--- WebApplication1/src/WebApplication1/Areas/MyOrders/Controllers/MyOrdersController.cs
+++ WebApplication1/src/WebApplication1/Areas/MyOrders/Controllers/MyOrdersController.cs
@@ -3,24 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using WebApplication1.Areas.MyOrders.DTOs;
 
 namespace WebApplication1.Areas.MyOrders.Controllers
 {
     [Route("api/myorders/")]
     public class MyOrdersController : Controller
     {
+        private IEnumerable<Order> _orders;
+
+
+        public MyOrdersController()
+        {
+            InitializeOrders();
+        }
+
+        private void InitializeOrders()
+        {
+            _orders = new[] { new Order { OrderNum = 1, ProductId = 1337, Qty = 1, Total = 50 },
+                              new Order { OrderNum = 2, ProductId = 42, Qty = 1, Total = 25 },
+                              new Order { OrderNum = 3, ProductId = 314, Qty = 2, Total = 17 },
+                              new Order { OrderNum = 4, ProductId = 451, Qty = 10, Total = 120 }
+                            };
+        }
+
         // GET: api/myorders
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Order> Get()
         {
-            return new string[] { "order1", "order2" };
+            return _orders;
         }
 
         // GET api/orders/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{orderNumber}")]
+        public Order Get(int orderNumber)
         {
-            return "order" + id;
+            return _orders.Where(o => o.OrderNum == orderNumber).FirstOrDefault();
         }
     }
 }
