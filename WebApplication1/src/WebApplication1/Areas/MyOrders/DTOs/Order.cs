89655a73a6ff0bb593ed4f98a515b3fb19a4d329
StarTrekRedneck <dippykdog@gmail.com>
2015-09-30 23:37:12 -0500

    Add order class in DTOs folder

---
+++ WebApplication1/src/WebApplication1/Areas/MyOrders/DTOs/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace WebApplication1.Areas.MyOrders.DTOs
+{
+    public class Order
+    {
+        public Int32 OrderNum { get; set; }
+        public String ProductId { get; set; }
+        public Int32 Qty { get; set; }
+        public Decimal Total { get; set; }
+    }
+}
