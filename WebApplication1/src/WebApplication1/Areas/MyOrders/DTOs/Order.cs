4e15c43fdd1ddc44f694b89ea928a55c7fb39a37
StarTrekRedneck <dippykdog@gmail.com>
2015-09-30 23:46:45 -0500

    Changed product id to Int32 on Order class

--- WebApplication1/src/WebApplication1/Areas/MyOrders/DTOs/Order.cs
+++ WebApplication1/src/WebApplication1/Areas/MyOrders/DTOs/Order.cs
@@ -6,7 +6,7 @@ namespace WebApplication1.Areas.MyOrders.DTOs
     public class Order
     {
         public Int32 OrderNum { get; set; }
-        public String ProductId { get; set; }
+        public Int32 ProductId { get; set; }
         public Int32 Qty { get; set; }
         public Decimal Total { get; set; }
     }
